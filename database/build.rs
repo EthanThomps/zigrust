54a6326ca977d5006366ffc5ec227d1940189de2
EthanThomps <81367830+EthanThomps@users.noreply.github.com>
2023-06-14 15:23:16 +0000

    125

--- database/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    cxx_build::bridge("src/main.rs")
-        .file("main.cpp")
-        .compile("client");
-}
-
--- database/src/main.rs
+++ database/src/main.rs
@@ -1,18 +1,4 @@
-use cxx::UniquePtr;
+
 fn main() {
     println!("Hello, world!");
-    ffi::hundred();
-}
-
-#[cxx::bridge]
-mod ffi {
-
-    unsafe extern "C++" {
-        include!("client.h");
-
-        type Hundred;
-
-        fn hundred() -> UniquePtr<Hundred>;
-
-    }
 }
